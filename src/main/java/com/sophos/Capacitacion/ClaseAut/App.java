8a1d8db70f1f50cd199fa55cffa67958f8777ab5
Contratista Sophos-Leonardo Rojas Escamilla <rojaleon@colpatria.com>
2020-06-21 10:05:56 -0500

    first commit

---
+++ src/main/java/com/sophos/Capacitacion/ClaseAut/App.java
@@ -0,0 +1,13 @@
+package com.sophos.Capacitacion.ClaseAut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
